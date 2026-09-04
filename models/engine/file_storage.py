e909960a500a3c8373cfeb8f3d610da1a7826951
KEFFALYSSA <keffalyssak@gmail.com>
2026-09-04 02:58:02 -0700

    Correct FileStorage class definition and private attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the FileStorage class for serializing and deserializing objects.
+This module defines the FileStorage class for serializing and deserializing.
 """
 import json
 from models.base_model import BaseModel
@@ -13,7 +13,7 @@ from models.review import Review
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
+    """Serializes instances to JSON file and deserializes JSON file to instances."""
     __file_path = "file.json"
     __objects = {}
 
