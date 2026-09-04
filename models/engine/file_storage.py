8e755fb76510708e24c58ebf23d0acc12d73cf98
KEFFALYSSA <keffalyssak@gmail.com>
2026-09-04 02:33:48 -0700

    Fix line length style error in FileStorage docstring

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,9 @@ from models.review import Review
 
 
 class FileStorage:
-    """Serializes instances to JSON file and deserializes JSON file to instances."""
+    """Serializes instances to JSON file and
+    deserializes JSON file to instances.
+    """
     __file_path = "file.json"
     __objects = {}
 
