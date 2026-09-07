ef72774ecc57ca7b27719b5db7c27a141a34bddb
keffalyssa <keffalyssak@gmail.com>
2026-09-07 03:40:09 -0700

    Add test package initialization files and comprehensive User unit tests

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""
-This module defines the State class.
-"""
+"""Defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Representation of State class."""
+    """Represent a State.
+
+    Attributes:
+        name (str): The name of the state.
+    """
     name = ""
