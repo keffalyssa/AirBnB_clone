56e36bd429da34333ae18423026067e1131305a0
keffalyssa <keffalyssak@gmail.com>
2026-09-07 03:35:03 -0700

    Fix package initialization and ensure User attributes are accessible

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Initialization file for the models package."""
+"""Initializes the models package."""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
